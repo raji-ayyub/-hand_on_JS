ecf3ab4119120aa0c4e33231966ade0b4cb87b55
raji-ayyub <ayyubraji87@gmail.com>
2026-01-20 12:31:59 +0100

    update

--- AJAX/script.js
+++ AJAX/script.js
@@ -1,5 +1,5 @@
 function getWeather() {
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=3cfa28092f25df600b1a9ba3865e3c47")
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=xxxxx")
     .then(response => response.json())
     .then(data => {
       document.getElementById("weather").innerText =
